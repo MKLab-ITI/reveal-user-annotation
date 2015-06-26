4b4e46e6d230e3d4827e3ae153ce59516137e4d2
georgiosrizos <georgevrizos@gmail.com>
2015-06-26 12:16:44 +0300

    Corrected package resources and removed configuration process.

--- reveal_user_annotation/__init__.py
+++ reveal_user_annotation/__init__.py
@@ -1,5 +1,1 @@
 __author__ = 'Georgios Rizos (georgerizos@iti.gr)'
-
-from reveal_user_annotation.configure import configure_package
-
-configure_package()
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='reveal-user-annotation',
-    version='0.1.17',
+    version='0.1.18',
     author='Georgios Rizos',
     author_email='georgerizos@iti.gr',
     packages=['reveal_user_annotation',
